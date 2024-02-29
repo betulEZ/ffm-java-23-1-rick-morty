069b32db378fc70511c1deb3a5b9167899496b8e
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-02-29 19:04:59 +0100

    add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import './App.css'
 import {useState} from "react";
 import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
+import {Link, Navigate, Route, Routes} from "react-router-dom";
 
 function App() {
 
     const [characters, setCharacters] = useState<Character[]>(charactersResponse.results)
 
+
     return (
         <>
-            <CharacterGallery  characters={characters}/>
+            <nav>
+                <Link to={"/Home/Welcome"}>Home Welcome Unsere Website</Link>
+                <br/>
+                <Link to={"/characters"}>Characters</Link>
+            </nav>
+            <Routes>
+                <Route path={"/"} element={<Navigate replace to="/Home/Welcome"/>}/>
+                <Route path="/characters" element={<CharacterGallery characters={characters}/>}/>
+            </Routes>
+
         </>
     )
 }
