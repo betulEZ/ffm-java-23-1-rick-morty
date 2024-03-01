211448e182f20591bb569a7e70e7d9592261250c
BetulEzgu <33276357+betulEZ@users.noreply.github.com>
2024-03-01 18:29:47 +0100

    add Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
-import {useState} from "react";
+import React, {useState} from "react";
 import {Character, charactersResponse} from "./characters.ts";
 import CharacterGallery from "./CharacterGallery.tsx";
 import {Link, Navigate, Route, Routes} from "react-router-dom";
 import CharacterDetails from "./CharacterDetails.tsx";
+import NewCharacter from "./NewCharacter.tsx";
+
 
 function App() {
 
@@ -16,9 +18,13 @@ function App() {
                 <Link to={"/Home/Welcome"}>Home Welcome Unsere Website</Link>
                 <br/>
                 <Link to={"/characters"}>Characters</Link>
+                <br/>
+                <Link to={"/newCharacters"}>Add new Characters</Link>
+
             </nav>
             <Routes>
                 <Route path={"/"} element={<Navigate replace to="/Home/Welcome"/>}/>
+                <Route path={"/newCharacters"}  element={<NewCharacter/>}/>
                 <Route path="/characters" element={<CharacterGallery characters={characters}/>}/>
                 <Route path={"/characters/:id"} element={<CharacterDetails characters={characters}/>}/>
             </Routes>
